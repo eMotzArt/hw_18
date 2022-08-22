6f3b7c4994324d5e9f14c13a68bbc907c8450a53
emotzart <johnshuabratsk@gmail.com>
2022-08-22 16:33:55 +0700

    added user api views

---
+++ app/views/users_api/users_api.py
@@ -0,0 +1,45 @@
+from flask_restx import Namespace, Resource, fields
+from app.views.users_api.parser import user_parser
+# from app.service import DirectorService
+
+api = Namespace('users')
+
+# api model
+user = api.model('User', {
+    'id': fields.Integer(readonly=True, description='User unique identifier'),
+    'username': fields.String(required=True, description='User name'),
+    'password': fields.String(required=True, description='User password'),
+    'role': fields.String(required=True, description='User role')
+})
+
+@api.route('/')
+class UsersView(Resource):
+    @api.marshal_list_with(user)
+    def get(self):
+        return # DirectorService().get_directors(), 200
+
+    @api.expect(user_parser)
+    @api.marshal_with(user, code=201)
+    def post(self):
+        return
+
+@api.route('/<int:pk>/')
+class UserView(Resource):
+    @api.marshal_with(user)
+    @api.response(code=404, description='User with this pk is not found in database')
+    def get(self, pk):
+        # if result := DirectorService().get_director_by_pk(pk):
+        #     return result, 200
+        return '', 404
+
+    @api.expect(user_parser)
+    @api.response(code=204, description="Successfully modified")
+    @api.marshal_with(user)
+    def put(self, pk):
+        # data = movie_parser.parse_args()
+        return # MovieDAO().update_item(pk, **data), 201
+
+    @api.response(code=204, description="Successfully deleted")
+    def delete(self, pk):
+        # MovieDAO().delete_item(pk)
+        return None, 204
